cd4fc8dbce1e0a407cd3211604d34589bd019022
fabianSiffert <siffert@sipgate.de>
2024-01-26 19:27:28 +0100

    ✨(main) basic browser router setup including 404 page

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Home from "./routes/Home/Home.tsx";
+import ErrorPage from "./routes/404/404.tsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+        errorElement: <ErrorPage />,
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
