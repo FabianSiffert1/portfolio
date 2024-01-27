b25e8fa5176225621a2b9b356262250eef750103
fabianSiffert <siffert@sipgate.de>
2024-01-27 23:39:08 +0100

    🐛(main) fix burger menu padding

--- src/Pages/Inventory/Inventory.tsx
+++ src/Pages/Inventory/Inventory.tsx
@@ -1,10 +1,5 @@
 import styles from './Inventory.module.scss'
 
-
 export default function Inventory() {
-  return (
-    <div className={styles.inventory}>
-      Inventory
-    </div>
-  )
+  return <div className={styles.inventory}>Inventory</div>
 }
