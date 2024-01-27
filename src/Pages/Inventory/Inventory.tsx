e32758d1e26f0d09c7ea13e287a9ed9aea89cfdf
fabianSiffert <siffert@sipgate.de>
2024-01-27 23:54:45 +0100

    🧑‍💻(main) add examples of handling mixins

--- src/Pages/Inventory/Inventory.tsx
+++ src/Pages/Inventory/Inventory.tsx
@@ -1,5 +1,9 @@
 import styles from './Inventory.module.scss'
 
 export default function Inventory() {
-  return <div className={styles.inventory}>Inventory</div>
+  return <div className={styles.inventory}>
+    <div className={styles.card}>
+      TODO: Inventory
+    </div>
+  </div>
 }
--- src/Pages/Market/Market.tsx
+++ src/Pages/Market/Market.tsx
@@ -1,12 +1,10 @@
-import styles from '../Home/Home.module.scss'
+import styles from './Market.module.scss';
 
 export default function Market() {
   return (
-    <div className={styles.container}>
+    <div className={styles.market}>
       <div className={styles.card}>
-        <h2>Welcome to the app</h2>
-        <p className={styles.text__primary}>Primary texts</p>
-        <p className={styles.text__secondary}>Secondary texts</p>
+        TODO: Market
       </div>
     </div>
   )
