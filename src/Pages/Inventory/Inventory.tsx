3abcd0841a5ae13edf14fbe15b43d858bdcdca10
fabianSiffert <siffert@sipgate.de>
2024-02-03 21:09:59 +0100

    ✨(feature/inventory-layout) add pokemon tcg api

--- src/Pages/Inventory/Inventory.tsx
+++ src/Pages/Inventory/Inventory.tsx
@@ -1,7 +1,12 @@
 import Card, { CardCategory, CardInterface } from '../../Components/Card'
+import pokemonTCGAPI from '../../util/api/pokemonTGC/pokemonTCGAPI'
 import styles from './Inventory.module.scss'
 
 export default function Inventory() {
+  pokemonTCGAPI.configure(import.meta.env.VITE_POKEMON_TCG_API_KEY)
+  pokemonTCGAPI.card.find('base1-4').then((card) => {
+    console.log(card.name)
+  })
   const cardAmount = 200
   const cardArray = []
   const cardArrayResult = getCardArray(cardAmount)
