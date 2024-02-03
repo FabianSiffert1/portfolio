71c36d650a4740269de4b402d8c1f7cabc3afa31
fabianSiffert <siffert@sipgate.de>
2024-02-03 23:30:42 +0100

    ✨(feature/pkeAPi) expand visual data representation

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,17 +1,13 @@
 import styles from './Card.module.scss'
 
-export enum CardCategory {
-  CHEAP = 'Cheap',
-  MID = 'Medium',
-  EXPENSIVE = 'Expensive'
-}
-
-export interface CardInterface {
+export interface CardProps {
   id?: string
   name?: string
-  category?: CardCategory
+  category?: string
   price?: number
   image?: string
+  setLogo?: string
+  setName?: string
 }
 
 function getCurrencyAndConvertPrice() {
@@ -19,14 +15,20 @@ function getCurrencyAndConvertPrice() {
   return ' €'
 }
 
-export default function Card(props: CardInterface) {
+export default function Card(props: CardProps) {
   return (
     <div className={styles.cardContainer}>
-      <div className={styles.card} style={props.image ? { backgroundImage: `url(${props.image})` } : undefined}>
-        Card {props.id ? props.id : null}
-      </div>
+      <div className={styles.card} style={props.image ? { backgroundImage: `url(${props.image})` } : undefined}></div>
       <div className={styles.cardInformationContainer}>
         <div className={styles.cardName}>{props.name ? props.name : null}</div>
+        <div className={styles.identifiers}>
+          id: {props.id ? props.id : null} set: {props.setName ? props.setName : null}{' '}
+          {props.setLogo ? (
+            <div className={styles.setLogo}>
+              <img src={props.setLogo} alt={'setLogo'} />
+            </div>
+          ) : null}
+        </div>
         <div className={styles.cardDescription}>{props.category ? props.category : null}</div>
         <div className={styles.cardPrice}>{props.price ? props.price.toString().concat(getCurrencyAndConvertPrice()) : null}</div>
       </div>
--- src/Pages/Inventory/Inventory.tsx
+++ src/Pages/Inventory/Inventory.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react'
-import Card, { CardCategory, CardInterface } from '../../Components/Card'
+import React, { ReactElement, useEffect, useState } from 'react'
+import Card from '../../Components/Card'
 import { PokemonCard } from '../../util/api/pokemonTGC/model/PokemonCard'
 import { PokemonSet } from '../../util/api/pokemonTGC/model/PokemonSet'
 import pokemonTCGAPI from '../../util/api/pokemonTGC/pokemonTCGAPI'
@@ -18,9 +18,6 @@ export default function Inventory() {
   const [areCardsLoading, setCardsLoading] = useState(true)
   const [areSetsLoading, setSetsLoading] = useState(true)
 
-  const cardAmount = 2
-  const cardArray = []
-
   const getBaseSet = () => {
     pokemonTCGAPI.set.all({ q: 'series:base' }).then((sets: PokemonSet[]) => {
       setSets(sets)
@@ -34,47 +31,31 @@ export default function Inventory() {
       setCardsLoading(false)
     })
   }
-
-  function createDummyCards() {
-    const cardArrayResult = getCardArray(cardAmount)
-    for (let i = 0; i < cardArrayResult.length; i++) {
-      cardArray.push(
-        <Card id={i} category={cardArrayResult[i].category} name={cardArrayResult[i].name} price={cardArrayResult[i].price} key={i} />
-      )
-    }
-  }
-
+  const cardArray: ReactElement[] = []
   if (!areSetsLoading && !areCardsLoading) {
-    cards.forEach((card) => cardArray.push(<Card id={card.id} name={card.name} image={card.images.large} key={card.id} />))
-    createDummyCards()
+    cards.forEach((card) => {
+      const price = card?.cardmarket?.prices?.avg30
+
+      cardArray.push(
+        <Card
+          id={card.id}
+          name={card.name}
+          image={card.images.large}
+          setName={card.set.name}
+          key={card.id}
+          price={price ? price : undefined}
+          setLogo={card.set.images.logo}
+        />
+      )
+    })
     return (
       <div className={styles.inventory}>
         {sets.length}
         <div className={styles.cardList}>{cardArray}</div>
+        <pre>{JSON.stringify(cards, null, 2)}</pre>
       </div>
     )
   } else {
     return <div className={styles.loadingState}>Fetching data...</div>
   }
-}
-
-function getCardArray(amountOfCardsToGenerate: number): CardInterface[] {
-  const cardHelperArray: CardInterface[] = []
-  for (let i = 0; i < amountOfCardsToGenerate; i++) {
-    cardHelperArray.push({
-      id: i.toString(),
-      category: randomEnum(CardCategory),
-      name: `Name ${i}`,
-      price: Math.floor(Math.random() * 100000) + 1
-    })
-  }
-  return cardHelperArray
-}
-
-function randomEnum<T>(anEnum: T): T[keyof T] {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-expect-error
-  const enumValues = Object.values(anEnum) as unknown as T[keyof T][]
-  const randomIndex = Math.floor(Math.random() * enumValues.length)
-  return enumValues[randomIndex]
 }
