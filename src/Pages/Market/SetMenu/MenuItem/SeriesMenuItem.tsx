5d7251c8abe04f42f5ccf40a8e82fef51ee70c1d
fabianSiffert <siffert@sipgate.de>
2024-02-07 16:35:56 +0100

    🚧(feature/pkeAPi) prevent multiple simultaneous series fetches

--- src/Pages/Market/SetMenu/MenuItem/SeriesMenuItem.tsx
+++ src/Pages/Market/SetMenu/MenuItem/SeriesMenuItem.tsx
@@ -1,34 +1,29 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { PokemonCard } from '../../../../util/api/pokemonTGC/model/PokemonCard'
-import { PokemonSet } from '../../../../util/api/pokemonTGC/model/PokemonSet'
 import pokemonTCGAPI from '../../../../util/api/pokemonTGC/pokemonTCGAPI'
 import styles from './SeriesMenuItem.module.scss'
 
-export interface SeriesSet {
-  series: string
-  symbol: string
-}
-
-export interface SeriesMenuProps {
+interface SeriesMenuProps {
   seriesName: string
   seriesSymbol: string
   key: number
   setCardList: (newCardList: PokemonCard[]) => void
+  setSeriesLoadingState: (isSeriesLoading: boolean) => void
+  isSeriesLoading: boolean
 }
 
-function SeriesMenuItem(props: SeriesMenuProps) {
-  const [seriesLoading, setSeriesLoading] = useState(false)
-
+export function SeriesMenuItem(props: SeriesMenuProps) {
   async function fetchSeries(pokemonSeries: string) {
-    setSeriesLoading(true)
+    console.log('Loading')
+    props.setSeriesLoadingState(true)
     try {
       pokemonSeries = pokemonSeries.replace(/^(.*?)\s.*$/, '$1')
       const cardList: PokemonCard[] = await pokemonTCGAPI.card.all({
         q: `set.series:${pokemonSeries}`,
         orderBy: '-cardmarket.prices.trendPrice'
       })
+      props.setSeriesLoadingState(false)
       props.setCardList(cardList)
-      setSeriesLoading(false)
     } catch (error) {
       console.error('Error fetching cards:', error)
       return []
@@ -39,37 +34,18 @@ function SeriesMenuItem(props: SeriesMenuProps) {
     <div
       key={props.id}
       className={styles.seriesMenuItem}
-      onClick={async () => {
-        await fetchSeries(props.seriesName)
-      }}
+      onClick={
+        !props.isSeriesLoading
+          ? async () => {
+              await fetchSeries(props.seriesName)
+            }
+          : undefined
+      }
     >
       <span className={styles.seriesImage}>
         <img src={props.seriesSymbol} alt={props.seriesName} />
       </span>
       <span className={styles.seriesName}>{props.seriesName}</span>
-      {seriesLoading ? <span className={styles.seriesLoading}>Fetching Cards....</span> : undefined}
     </div>
   )
-}
-
-export function createSetMenuList(
-  pokemonSets: PokemonSet[],
-  uniquePokemonSeriesMap: Record<string, string>,
-  seriesArray: React.ReactElement<SeriesSet>[],
-  setCardList: (newCardList: PokemonCard[]) => void
-) {
-  pokemonSets.forEach((set) => {
-    if (!Object.prototype.hasOwnProperty.call(uniquePokemonSeriesMap, set.series)) {
-      uniquePokemonSeriesMap[set.series] = set.images.symbol
-    }
-  })
-  const uniqueSeriesArray: {
-    series: string
-    imageLink: string
-  }[] = Object.entries(uniquePokemonSeriesMap).map(([series, imageLink]) => ({ series, imageLink }))
-  let id = 0
-  uniqueSeriesArray.forEach((series) => {
-    seriesArray.push(<SeriesMenuItem seriesName={series.series} seriesSymbol={series.imageLink} key={id} setCardList={setCardList} />)
-    id++
-  })
 }
--- src/Pages/Market/SetMenu/SeriesMenu.tsx
+++ src/Pages/Market/SetMenu/SeriesMenu.tsx
@@ -1,19 +1,66 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useState } from 'react'
 import { PokemonCard } from '../../../util/api/pokemonTGC/model/PokemonCard'
 import { PokemonSet } from '../../../util/api/pokemonTGC/model/PokemonSet'
-import { createSetMenuList, SeriesSet } from './MenuItem/SeriesMenuItem'
+import { SeriesMenuItem } from './MenuItem/SeriesMenuItem'
 import styles from './SeriesMenu.module.scss'
 
-export interface SetMenuProps {
+interface SetMenuProps {
   pokemonSets: PokemonSet[]
   setCardList: (newCardList: PokemonCard[]) => void
+}
+
+export interface SeriesSet {
+  series: string
+  symbol: string
 }
 
 export default function SeriesMenu(props: SetMenuProps) {
   const seriesArray: ReactElement<SeriesSet>[] = []
   const pokemonSetSeriesSet = new Set<SeriesSet>()
 
-  createSetMenuList(props.pokemonSets, pokemonSetSeriesSet, seriesArray, props.setCardList)
+  const [seriesLoading, setSeriesLoading] = useState(false)
+  const setLoadingState = (isLoading: boolean) => {
+    setSeriesLoading(isLoading)
+  }
+  createSetMenuList(props.pokemonSets, pokemonSetSeriesSet, seriesArray, props.setCardList, setLoadingState, seriesLoading)
 
-  return <div className={styles.setMenu}>{seriesArray}</div>
+  return (
+    <div className={styles.seriesMenuContainer}>
+      <span className={styles.seriesMenu}>{seriesArray}</span>
+      {seriesLoading && <span className={styles.seriesLoadingIndicator}>Fetching Cards...</span>}
+    </div>
+  )
+}
+
+function createSetMenuList(
+  pokemonSets: PokemonSet[],
+  uniquePokemonSeriesMap: Record<string, string>,
+  seriesArray: React.ReactElement<SeriesSet>[],
+  setCardList: (newCardList: PokemonCard[]) => void,
+  setSeriesLoadingState: (isLoading: boolean) => void,
+  isSeriesLoading: boolean
+) {
+  pokemonSets.forEach((set) => {
+    if (!Object.prototype.hasOwnProperty.call(uniquePokemonSeriesMap, set.series)) {
+      uniquePokemonSeriesMap[set.series] = set.images.symbol
+    }
+  })
+  const uniqueSeriesArray: {
+    series: string
+    imageLink: string
+  }[] = Object.entries(uniquePokemonSeriesMap).map(([series, imageLink]) => ({ series, imageLink }))
+  let id = 0
+  uniqueSeriesArray.forEach((series) => {
+    seriesArray.push(
+      <SeriesMenuItem
+        seriesName={series.series}
+        seriesSymbol={series.imageLink}
+        key={id}
+        setCardList={setCardList}
+        setSeriesLoadingState={setSeriesLoadingState}
+        isSeriesLoading={isSeriesLoading}
+      />
+    )
+    id++
+  })
 }
