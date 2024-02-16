98e0c365bd99c59aa0d73796c3ac7e8b1de20d9a
fabianSiffert <siffert@sipgate.de>
2024-02-16 10:46:36 +0100

    ♻️🐛(main) improve mobile nav menu & disable scrolling when mobile nav menu is open

--- src/Header/Navigation/MobileNavigation/MobileNavigation.tsx
+++ src/Header/Navigation/MobileNavigation/MobileNavigation.tsx
@@ -1,34 +1,42 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import ThemeToggle from '../../ThemeToggle/ThemeToggle'
 import styles from './MobileNavigation.module.scss'
 
 export default function MobileNavigation() {
-  const [hamburgerOpen, setHamburgerOpen] = useState(false)
+  const [hamburgerMenuOpen, setHamburgerMenuOpen] = useState(false)
 
-  const toggleHamburger = () => {
-    setHamburgerOpen(!hamburgerOpen)
+  const toggleHamburgerMenu = () => {
+    setHamburgerMenuOpen(!hamburgerMenuOpen)
   }
 
+  useEffect(() => {
+    if (hamburgerMenuOpen) {
+      document.body.classList.add('no-scroll')
+    } else {
+      document.body.classList.remove('no-scroll')
+    }
+    
+    return () => {
+      document.body.classList.remove('no-scroll')
+    }
+  }, [hamburgerMenuOpen])
+
   return (
-    <div className={styles.mobileNavigation} onClick={toggleHamburger}>
+    <div className={styles.mobileNavigation} onClick={toggleHamburgerMenu}>
       <div className={styles.burger} />
       <div className={styles.burger} />
       <div className={styles.burger} />
-      <div className={styles.hamburgerMenu} style={{ display: hamburgerOpen ? 'inline' : 'none' }}>
-        <nav>
-          <ul>
-            <li key={'inventory'}>
-              <Link to={`/`}>Inventory</Link>
-            </li>
-            <li key={'market'}>
-              <Link to={`market`}>Market</Link>
-            </li>
-            <li key={'themeToggle'}>
-              <ThemeToggle />
-            </li>
-          </ul>
-        </nav>
+      <div className={styles.navigationMenu} style={{ display: hamburgerMenuOpen ? 'flex' : 'none' }}>
+        <div className={styles.link}>
+          <Link to={`/`}>Inventory</Link>
+        </div>
+        <div className={styles.link}>
+          <Link to={`market`}>Market</Link>
+        </div>
+        <div className={styles.linkImage}>
+          <ThemeToggle />
+        </div>
       </div>
     </div>
   )
