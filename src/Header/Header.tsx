6d86427ce498c74770838c125d2b7b6e0778f4e1
fabianSiffert <siffert@sipgate.de>
2024-02-16 11:27:07 +0100

    ✨(main) disable scrolling when in mobile menu

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,11 +1,31 @@
+import { useLayoutEffect, useRef } from 'react'
 import styles from './Header.module.scss'
 import MobileNavigation from './Navigation/MobileNavigation/MobileNavigation'
 import WebNavigation from './Navigation/WebNavigation/WebNavigation'
 import Title from './Title/Title'
 
 export default function Header() {
+  const componentRef = useRef<HTMLDivElement>(null)
+  const handleDisplayStyleChange = () => {
+    const widthThreshold = 1536
+
+    if (componentRef.current?.offsetWidth && componentRef.current.offsetWidth < widthThreshold) {
+      document.body.classList.remove('no-scroll')
+    }
+  }
+  useLayoutEffect(() => {
+    const observer = new ResizeObserver(() => {
+      handleDisplayStyleChange()
+    })
+    if (componentRef.current) {
+      observer.observe(componentRef.current)
+    }
+
+    return () => observer.disconnect()
+  }, [])
+
   return (
-    <div className={styles.header}>
+    <div className={styles.header} ref={componentRef}>
       <div className={styles.title}>
         <Title />
       </div>
