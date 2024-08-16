24255f26accec656509e4fc192d9fef1a8470e64
fabianSiffert <siffert@sipgate.de>
2024-08-16 12:06:18 +0200

    ♻️🔥(main): start deleting tgc related stuff and remove Headercontext and headerMiddleItem

--- src/Pages/Inventory/Inventory.tsx
+++ src/Pages/Inventory/Inventory.tsx
@@ -1,16 +1,10 @@
-import React, { useContext, useEffect } from 'react'
-import { HeaderContext } from '../../Header/HeaderProvider'
+import React from 'react'
 import styles from './Inventory.module.scss'
 
 export default function Inventory() {
-  const headerContext = useContext(HeaderContext)
-  useEffect(() => {
-    headerContext.setHeaderItem(<></>)
-  }, [])
   return (
     <div className={styles.inventory}>
-      A Placeholder for a Inventory that holds all cards, preferably with condition, price at time of purchase and current price pulled from
-      an API.
+      A Placeholder for another Screen
     </div>
   )
 }
