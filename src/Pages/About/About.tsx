8672159cc300a6d49b61e2af740707b890cf8646
fabianSiffert <siffert@sipgate.de>
2024-11-08 14:55:05 +0100

    🚧(main): start implementing skills section

--- src/Pages/About/About.tsx
+++ src/Pages/About/About.tsx
@@ -2,15 +2,22 @@ import React from 'react'
 import styles from './About.module.scss'
 import ProfileSection from './components/ProfileSection/ProfileSection'
 import SocialsSection from './components/SocialsSection/SocialsSection'
+import SkillsSection from './components/SkillsSection/SkillsSection'
 
 
 export default function About() {
 
   return (
     <div className={styles.about}>
-      <ProfileSection />
-      <SocialsSection />
-      <div/>
+      <div className={styles.profileAndSocials}>
+        <ProfileSection />
+        <SocialsSection />
+      </div>
+      <div className={styles.spacer}/>
+      <div className={styles.skills}>
+        <SkillsSection />
+      </div>
+      <div />
     </div>
   )
 }
---
+++ src/Pages/About/components/SkillsSection/SkillsSection.tsx
@@ -0,0 +1,11 @@
+import styles from './SkillsSection.module.scss'
+
+
+export default function SkillsSection() {
+
+  return (
+    <div className={styles.skillsSection}>
+      Skills
+    </div>
+  )
+}
