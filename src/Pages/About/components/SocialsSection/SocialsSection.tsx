711aaaf4579452b9a17ac320b41a4439b7800643
fabianSiffert <siffert@sipgate.de>
2024-11-08 14:29:20 +0100

    🚧(main): add harcoded links to socials

--- src/Pages/About/components/SocialsSection/SocialsSection.tsx
+++ src/Pages/About/components/SocialsSection/SocialsSection.tsx
@@ -1,13 +1,24 @@
 import styles from './SocialsSection.module.scss'
 
-
 export default function SocialsSection() {
 
   return (
     <div className={styles.socialsSection}>
-      <div className={styles.email}>📧 Email</div>
-      <div className={styles.github}>🧑‍💻 Github</div>
-      <div className={styles.linkedIn}>🤝 LinkedIn</div>
+      <div className={styles.email}>
+        <a href={"mailto:siffert.dev+io@gmail.com"}>
+        📧 Email
+        </a>
+      </div>
+      <div className={styles.github}>
+        <a href={'https://github.com/FabianSiffert1'} target="_blank">
+          🧑‍💻 Github
+        </a>
+        </div>
+      <div className={styles.linkedIn}>
+        <a href={"https://www.linkedin.com/in/fabian-siffert-5b2481289/"}target="_blank">
+          🤝 LinkedIn
+        </a>
+      </div>
     </div>
-  )
+)
 }
