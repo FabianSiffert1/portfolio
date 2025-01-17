ad6075d20a0cc5e5ccef930d3f0b1245d6968e64
fabianSiffert <siffert@sipgate.de>
2025-01-17 11:31:07 +0100

    🚧(main): reduce title and footer font size

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -2,9 +2,8 @@ import styles from './Footer.module.scss'
 
 export default function Footer() {
   return (
-    // remove style tag if footer is needed
     <div className={styles.footer}>
-      <a href={'https://siffert.io'}>© fabianSiffert</a>
+      <a href={'https://siffert.io'}>©</a>
       &nbsp;
       {new Date().getFullYear().toString()}
     </div>
