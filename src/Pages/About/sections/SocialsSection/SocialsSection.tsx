de54a6a47dccdd2e604ed509079bb5634ab516a4
fabianSiffert <siffert@sipgate.de>
2025-01-31 14:52:29 +0100

    💄separate socials sections by background color

--- src/Pages/About/sections/SocialsSection/SocialsSection.tsx
+++ src/Pages/About/sections/SocialsSection/SocialsSection.tsx
@@ -6,26 +6,32 @@ export default function SocialsSection() {
     <Section title="Get in Touch">
       {SocialSectionContent}
     </Section>
-  );
+  )
 }
 
 const SocialSectionContent = (
   <div className={styles.socialsSection}>
-    <div className={styles.email}>
-      <a href={'mailto:siffert.dev+io@gmail.com'}>
-        Email
-      </a>
+    <div className={styles.socialLink}>
+      <div className={styles.email}>
+        <a href={'mailto:siffert.dev+io@gmail.com'}>
+          Email
+        </a>
+      </div>
     </div>
-    <div className={styles.linkedIn}>
-      <a href={'https://www.linkedin.com/in/fabian-siffert-5b2481289/'} target="_blank">
-        LinkedIn
-      </a>
+    <div className={styles.socialLink}>
+      <div className={styles.linkedIn}>
+        <a href={'https://www.linkedin.com/in/fabian-siffert-5b2481289/'} target="_blank">
+          LinkedIn
+        </a>
+      </div>
     </div>
-    <div className={styles.github}>
-      <a href={'https://github.com/FabianSiffert1'} target="_blank">
-        Github
-      </a>
+    <div className={styles.socialLink}>
+      <div className={styles.github}>
+        <a href={'https://github.com/FabianSiffert1'} target="_blank">
+          Github
+        </a>
+      </div>
     </div>
   </div>
-);
+)
 
