23e00266df97239f42cd20583b4904b9dc3e4884
fabianSiffert <siffert@sipgate.de>
2025-01-31 15:35:52 +0100

    💄 minor ui changes

--- src/Pages/About/sections/SkillsSection/SkillsSection.tsx
+++ src/Pages/About/sections/SkillsSection/SkillsSection.tsx
@@ -5,40 +5,55 @@ import Section from '../../../../ui/components/Section'
 export default function SkillsSection() {
 
   return (
-    <Section title="Skills" >
+    <Section title="Skills">
       {SkillsSectionContent}
     </Section>
   )
 }
 
 const SkillsSectionContent = (
-    <div className={styles.skillsSection}>
+  <div className={styles.skillsSection}>
 
+    <div className={styles.main}>
       <Section title="mainly:">
-      <div className={styles.androidDevelopment}>
-        <div className={styles.android}>
-          Android
+        <div className={styles.androidDevelopment}>
+          <div className={styles.android}>
+            Android
+          </div>
+          <div className={styles.kotlin}>
+            Kotlin
+          </div>
+          <div className={styles.jetpackCompose}>
+            Jetpack Compose
+          </div>
         </div>
-        <div className={styles.kotlin}>
-          Kotlin
-        </div>
-        <div className={styles.jetpackCompose}>
-          Jetpack Compose
-        </div>
-      </div>
       </Section>
-
-      <Section title="Also:">
-      <div className={styles.otherSkills}>
-        <div className={styles.react}>React</div>
-        <div className={styles.typeScript}>TypeScript</div>
-        <div className={styles.sass}>Sass</div>
-        <div className={styles.java}>Java</div>
-        <div className={styles.javaScript}>JavaScript</div>
-      </div>
+      <div className={styles.spacer}/>
+      <Section title={'and:'}>
+        <div className={styles.softSkills}>
+        <div className={styles.scrum}>
+          Scrum
+        </div>
+          <div className={styles.po}>
+            Product Owner Certificate
+          </div>
+        </div>
       </Section>
-
     </div>
+
+    <div className={styles.subSkills}>
+      <Section title="Also:">
+        <div className={styles.otherSkills}>
+          <div className={styles.react}>React</div>
+          <div className={styles.typeScript}>TypeScript</div>
+          <div className={styles.sass}>Sass</div>
+          <div className={styles.java}>Java</div>
+          <div className={styles.javaScript}>JavaScript</div>
+        </div>
+      </Section>
+    </div>
+
+  </div>
 
 )
 
