add623ce64624411d50b28e5ad27b45d47bc0d66
FabianSiffert1 <siffert@sipgate.de>
2025-03-27 15:33:03 +0100

    💄modify layout, use less colors

--- src/Pages/About/sections/CareerSection/CareerSection.tsx
+++ src/Pages/About/sections/CareerSection/CareerSection.tsx
@@ -28,7 +28,7 @@ const CareerSectionContent = (
       </div>
     </div>
     <div className={styles.employment}>
-      <div className={styles.timespan}>2021-2024</div>
+      <div className={styles.timespan}>2021-Jan.2024</div>
       <div className={styles.job}>Apprenticeship: IT specialist in application development</div>
       <div className={styles.employer}>
         <Link to={'https://www.sipgate.de/'} target='_blank'>
@@ -37,7 +37,7 @@ const CareerSectionContent = (
       </div>
     </div>
     <div className={styles.employment}>
-      <div className={styles.timespan}>2024-Now</div>
+      <div className={styles.timespan}>Jan.2024-Now</div>
       <div className={styles.job}> Full time android/software developer</div>
       <div className={styles.employer}>
         <a href={'https://sipgate.de'} target='_blank'>
--- src/Pages/About/sections/SkillsSection/SkillsSection.tsx
+++ src/Pages/About/sections/SkillsSection/SkillsSection.tsx
@@ -2,39 +2,32 @@ import styles from './SkillsSection.module.scss'
 import Section from '../../../../ui/components/Section'
 
 export default function SkillsSection() {
-  return <Section title='Skills'>{SkillsSectionContent}</Section>
+    return <Section title='Skills'>{SkillsSectionContent}</Section>
 }
 
 const SkillsSectionContent = (
-  <div className={styles.skillsSection}>
-    <div className={styles.main}>
-      <Section title='mainly:'>
-        <div className={styles.androidDevelopment}>
-          <div className={styles.android}>Android</div>
-          <div className={styles.kotlin}>Kotlin</div>
-          <div className={styles.jetpackCompose}>Jetpack Compose</div>
+    <div className={styles.skillsSection}>
+
+        <div className={styles.mainProgrammingSkills}>
+            <Section title='mainly:'>
+                <div className={styles.android}>Android</div>
+                <div className={styles.kotlin}>Kotlin</div>
+                <div className={styles.jetpackCompose}>Jetpack Compose</div>
+            </Section>
         </div>
-      </Section>
-
-      <div className={styles.spacer} />
-
-      <Section title={'and:'}>
+        <div className={styles.secondaryProgrammingSkills}>
+            <Section title='Also:'>
+                <div className={styles.react}>React</div>
+                <div className={styles.typeScript}>Type- & JavaScript</div>
+                <div className={styles.sass}>Css & Sass</div>
+                <div className={styles.java}>Java</div>
+            </Section>
+        </div>
         <div className={styles.softSkills}>
-          <div className={styles.scrum}>Scrum</div>
-          <div className={styles.po}>Product Owner Certificate</div>
+            <Section title={'and:'}>
+                <div className={styles.scrum}>Scrum</div>
+                <div className={styles.po}>Product Owner Certificate</div>
+            </Section>
         </div>
-      </Section>
     </div>
-
-    <div className={styles.subSkills}>
-      <Section title='Also:'>
-        <div className={styles.otherSkills}>
-          <div className={styles.react}>React</div>
-          <div className={styles.typeScript}>Type- & JavaScript</div>
-          <div className={styles.sass}>Css & Sass</div>
-          <div className={styles.java}>Java</div>
-        </div>
-      </Section>
-    </div>
-  </div>
 )
