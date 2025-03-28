b8b3ffde026493bcc065ecdb2adcebc16aaa6e31
FabianSiffert1 <siffert@sipgate.de>
2025-03-28 14:29:36 +0100

    🐛 set vite.config.ts port to 3000

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: { port: 3030 },
+  server: { port: 3000 },
   plugins: [react()],
   css: {
     preprocessorOptions: {
