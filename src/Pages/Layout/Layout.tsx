ebda4d8a12ac5d9dfa0a4b24b47d565f243a04fe
FabianSiffert1 <siffert@sipgate.de>
2025-03-28 15:57:09 +0100

    🔥 remove copyright footer

--- src/Pages/Layout/Layout.tsx
+++ src/Pages/Layout/Layout.tsx
@@ -1,6 +1,5 @@
 import { useContext, useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
-import Footer from '../../Footer/Footer'
 import Header from '../../Header/Header'
 import { ThemeContext } from '../../ui/theme/ThemeProvider'
 import styles from './Layout.module.scss'
@@ -18,9 +17,6 @@ export default function Layout() {
       </div>
       <div className={styles.pageContainer}>
         <Outlet />
-      </div>
-      <div className={styles.footer}>
-        <Footer />
       </div>
     </div>
   )
