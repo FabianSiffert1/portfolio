790897e470eea0cc4ca88d59076a09bc9c522fbb
FabianSiffert1 <siffert@sipgate.de>
2025-03-28 11:31:47 +0100

    💄 improve projects layout

--- src/Pages/Projects/Projects.tsx
+++ src/Pages/Projects/Projects.tsx
@@ -1,14 +1,31 @@
 import React from 'react'
 import styles from './Projects.module.scss'
-import Project from "./project/Project";
+import Project, {ColorVariant} from "./project/Project";
 
 export default function Projects() {
-  return <div className={styles.projects}>
-    <Project projectTitle={"siffert.io 1.0"}
-             projectDescription={"This is one of two personal portfolios I’ve created.\n Built during my apprenticeship, it reflects my early exploration of web development with a focus on simplicity.\n Since then, I’ve created a second portfolio—more minimalistic in design and built with responsiveness in mind — which you’re currently viewing."}
-             links={[
-               {title: "Deployment", url: "https://old.siffert.io"},
-               {title: "Github", url: "https://github.com/FabianSiffert1/siffertio"}
-             ]}/>
-  </div>
+    return <div className={styles.projects}>
+        <Project projectTitle={"siffert.io 2.0"}
+                 projectDescription={"This is the newer of two portfolios — minimalistic and responsive, reflecting my current approach to design. However, I’m not a web developer by trade; I focus on Android development."}
+                 links={[
+                     {title: "Deployment", url: "https://siffert.io"},
+                     {title: "Github", url: "https://github.com/FabianSiffert1/portfolio"}
+                 ]}/>
+        <Project projectTitle={"siffert.io 1.0"}
+                 projectDescription={"This is one of two personal portfolios I’ve created.\n Built during my apprenticeship, it reflects my early exploration of web development with a focus on simplicity.\n Since then, I’ve created a second portfolio—more minimalistic in design and built with responsiveness in mind — which you’re currently viewing."}
+                 links={[
+                     {title: "Deployment", url: "https://old.siffert.io"},
+                     {title: "Github", url: "https://github.com/FabianSiffert1/siffertio"}
+                 ]}
+            backgroundColor={ColorVariant.elementPink}
+        />
+        <Project projectTitle={"inv"}
+                 projectDescription={"A work-in-progress™, mobile-first website that originally started as a portfolio redesign.\n Over time, it evolved into a Pokémon Trading Card Game platform to help me sell my childhood collection at fair prices."}
+                 links={[
+                     {title: "Deployment", url: "https://inv.siffert.io"},
+                     {title: "Github", url: "https://github.com/FabianSiffert1/inv"}
+                 ]}
+                 backgroundColor={ColorVariant.elementGreenVariant2}
+        />
+        <div className={styles.spacer}/>
+    </div>
 }
--- src/Pages/Projects/project/Project.tsx
+++ src/Pages/Projects/project/Project.tsx
@@ -2,7 +2,6 @@ import React, {JSX} from "react";
 import styles from "./Project.module.scss";
 import {Link} from "react-router-dom";
 import Section from "../../../ui/components/Section";
-
 interface ProjectLink {
     title: string;
     url: string;
@@ -12,12 +11,22 @@ interface ProjectSectionProps {
     projectTitle: string;
     projectDescription: string | JSX.Element;
     links: ProjectLink[];
+    backgroundColor?: ColorVariant,
 }
 
-const ProjectSection: React.FC<ProjectSectionProps> = ({ projectTitle, projectDescription, links }) => {
+export enum ColorVariant {
+    elementBlue = "elementBlue",
+    elementPink = "elementPink",
+    elementGreenVariant2 = "elementGreenVariant2"
+}
+
+const ProjectSection: React.FC<ProjectSectionProps> = ({ projectTitle, projectDescription, links, backgroundColor = ColorVariant.elementBlue}) => {
+    const backgroundStyle = {
+        backgroundColor: `color-mix(in srgb, var(--${backgroundColor}) 35%, transparent)`,
+    };
     return (
         <Section title={projectTitle}>
-            <div className={styles.project}>
+            <div className={styles.project} style={backgroundStyle}>
                 <div className={styles.description}>
                     {typeof projectDescription === "string" ? (
                         projectDescription.split("\n").map((line, idx) => (
@@ -30,10 +39,9 @@ const ProjectSection: React.FC<ProjectSectionProps> = ({ projectTitle, projectDe
                         projectDescription
                     )}
                 </div>
-                <div className={styles.spacer} />
                 <div className={styles.links}>
                     {links.map((link, index) => (
-                        <Link key={index} to={link.url} target="_blank">
+                        <Link key={index} to={link.url} target="_blank" style={backgroundStyle}>
                             {link.title}
                         </Link>
                     ))}
