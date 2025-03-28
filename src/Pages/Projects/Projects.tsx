1019f380cc761bc85ba4117fc387d9518b159dc2
FabianSiffert1 <siffert@sipgate.de>
2025-03-28 16:23:52 +0100

    💬 replace deployment with link

--- src/Pages/Projects/Projects.tsx
+++ src/Pages/Projects/Projects.tsx
@@ -7,13 +7,13 @@ export default function Projects() {
         <Project projectTitle={"siffert.io 2.0"}
                  projectDescription={"This is my current portfolio—more minimalistic in design and built with responsiveness in mind."}
                  links={[
-                     {title: "Deployment", url: "https://siffert.io"},
+                     {title: "Link", url: "https://siffert.io"},
                      {title: "Github", url: "https://github.com/FabianSiffert1/portfolio"}
                  ]}/>
         <Project projectTitle={"siffert.io 1.0"}
                  projectDescription={"This is the first of two personal portfolios I’ve created.\n Built during my apprenticeship, it reflects my early exploration of web development with a focus on simplicity.\n"}
                  links={[
-                     {title: "Deployment", url: "https://old.siffert.io"},
+                     {title: "Link", url: "https://old.siffert.io"},
                      {title: "Github", url: "https://github.com/FabianSiffert1/siffertio"}
                  ]}
             backgroundColor={ColorVariant.elementPink}
@@ -21,7 +21,7 @@ export default function Projects() {
         <Project projectTitle={"inv"}
                  projectDescription={"A work-in-progress™, mobile-first website that originally started as a portfolio redesign.\n Over time, it evolved into a Pokémon Trading Card Game platform to help me sell my childhood collection at fair prices."}
                  links={[
-                     {title: "Deployment", url: "https://inv.siffert.io"},
+                     {title: "Link", url: "https://inv.siffert.io"},
                      {title: "Github", url: "https://github.com/FabianSiffert1/inv"}
                  ]}
                  backgroundColor={ColorVariant.elementGreenVariant2}
