8f1503e4a30ca2c7fc04c0ef8028ddd24a2bdaed
FabianSiffert1 <siffert@sipgate.de>
2025-05-21 09:38:29 +0200

    💬 update inventory project description

--- src/Pages/Projects/Projects.tsx
+++ src/Pages/Projects/Projects.tsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import styles from './Projects.module.scss'
 import Project, {ColorVariant} from "./project/Project";
+import {Link} from "react-router-dom";
 
 export default function Projects() {
     return <div className={styles.projects}>
-        <Project projectTitle={"Inventory"}
-                 projectDescription={"A simple, Android-only investment tracker.\n\n" +
-                     "This is the project I'm currently working on. As of May 2025, it runs on a local database, which I plan to replace in the future. The project hasn't been deployed yet, as it's still a work in progress."}
-                 links={[
-                     {title: "Github", url: "https://github.com/FabianSiffert1/inventory"}
-                 ]}/>
+        <Project
+            projectTitle="Inventory"
+            projectDescription={
+                <>
+                    A simple, Android-only investment tracker.<br/><br/>
+                    This is the project I'm currently working on, built using best practices recommended by Google's{" "}
+                    <Link to="https://github.com/android/nowinandroid" target="_blank" style={{fontWeight: 550}}>
+                    NowInAndroid
+                    </Link> and informed by my professional experience. <br/><br/>
+                    As of May 2025, it runs on a local database, which I plan to replace in the future.
+                    The project hasn't been deployed yet, as it's still a work in progress.
+                </>
+            }
+            links={[
+                { title: "Github", url: "https://github.com/FabianSiffert1/inventory" }
+            ]}
+        />
         <Project projectTitle={"inv"}
                  projectDescription={"A work-in-progress™ (read: very rough, and likely never to be finished), mobile-first website that originally began as a portfolio redesign. Over time, it evolved into a Pokémon Trading Card Game platform to help me sell my childhood collection at fair prices."}
                  links={[
