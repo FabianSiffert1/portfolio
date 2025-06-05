395cdab57bcb2743f1f7df3c91b9cf66c8562367
FabianSiffert1 <siffert@sipgate.de>
2025-06-05 11:38:27 +0200

    💄 refactor SkillsSection so that section titles are displayed on top

--- src/Pages/About/sections/SkillsSection/SkillsSection.tsx
+++ src/Pages/About/sections/SkillsSection/SkillsSection.tsx
@@ -2,7 +2,39 @@ import styles from './SkillsSection.module.scss'
 import Section from '../../../../ui/components/Section'
 
 export default function SkillsSection() {
-    return <Section title='Skills'>{SkillsSectionContent}</Section>
+    return <div className={styles.skillsSection}>
+
+        <div className={styles.mainProgrammingSkills}>
+            <Section title='Main Skills:'>
+                <div className={styles.mainProgrammingSkillsList}>
+                    <div className={styles.android}>Android</div>
+                    <div className={styles.kotlin}>Kotlin</div>
+                    <div className={styles.jetpackCompose}>Jetpack Compose</div>
+                </div>
+            </Section>
+        </div>
+        <div className={styles.secondaryProgrammingSkills}>
+            <Section title='Also:'>
+                <div className={styles.secondaryProgrammingSkillsList}>
+                    <div className={styles.react}>React</div>
+                    <div className={styles.typeScript}>Type- & JavaScript</div>
+                    <div className={styles.sass}>Css & Sass</div>
+                    <div className={styles.java}>Java</div>
+                </div>
+            </Section>
+        </div>
+        <div className={styles.softSkills}>
+            <Section title={'and:'}>
+                <div className={styles.softSkillsList}>
+                    <div className={styles.scrum}>Scrum</div>
+                    <div className={styles.po}>Product Owner Certificate*</div>
+                    <div className={styles.pottery}>Pottery</div>
+                    <div className={styles.bouldering}>Bouldering</div>
+                    <div className={styles.deprecated}>* Expired Dec. 2024</div>
+                </div>
+            </Section>
+        </div>
+    </div>
 }
 
 const SkillsSectionContent = (
